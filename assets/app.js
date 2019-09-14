128cd14d6b1bcca6b19a8812a9b7962095fb86fe
Sergey Sokurenko <s.sokurenko@gmail.com>
2019-09-14 10:44:34 +0200

    Added JS examples

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,18 @@
-console.log('Welcome to JavaScript');
+const showMessage = text => `Welcome ${text}`;
+
+const functionWithCallBack = callback => {
+  console.log(callback('Mario'));
+}
+
+// Async code
+window.setTimeout(
+  () => console.log(showMessage('Luigi')),
+  3000);
+
+window.setInterval(
+  () => console.log(new Date()),
+  1000
+)
+
+// Sync code
+functionWithCallBack(showMessage);
