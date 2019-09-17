1d714791572f7a71761dcb1443c1afa677bf7a95
Vladislav <Vlidik1345@gmail.com>
2019-09-17 16:33:03 +0300

    Add function to button and add real time to DOM

--- assets/app.js
+++ assets/app.js
@@ -9,10 +9,10 @@ window.setTimeout(
   () => console.log(showMessage('Luigi')),
   3000);
 
-window.setInterval(
+/*window.setInterval(
   () => console.log(new Date()),
   1000
-)
+)*/
 
 // Sync code
 functionWithCallBack(showMessage);
@@ -23,16 +23,47 @@ const showJoke = text => {
 }
 
 // Fetch API example
+
+let btn = document.querySelector('.container .btn');
 const apiURl = 'https://api.chucknorris.io/jokes/random';
 
-showJoke('Loading...');
+btn.onclick = () => {
+  showJoke('Loading...');
 
-window.fetch(apiURl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    if (data && data.value) {
-      showJoke(data.value);
-    }
-  });
+  window.fetch(apiURl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data && data.value) {
+        showJoke(data.value);
+      }
+    });
+}
+
+let clock = document.querySelector('.time');
+
+let clockTimer = () => {
+  let time = new Date();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+
+  if (hours < 10) {
+    hours = '0' + hours;
+  }else if (minutes < 10){
+    minutes = '0' + minutes;
+  }else if (seconds < 10){
+    seconds = '0' + seconds;
+  }
+
+  let timerString = hours + ':' + minutes + ':' + seconds;
+
+  clock.textContent = timerString;
+}
+
+clockTimer();
+setInterval(clockTimer, 1000);
+
+
+
